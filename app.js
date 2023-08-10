c889525396ebd8d6fbbd495080277ca31c36c5ab
arjablc <arjablc@gmail.com>
2023-08-10 20:52:27 +0545

    homepage and add blog form added

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 
-//setting the view engine to use ejs 
+//setting the view engine to use ejs
 app.set("View Engine", "ejs");
 
-app.get("/", (req, res )=>{
-    res.render("home.ejs");
-})
+app.get("/", (req, res) => {
+	res.render("./home/home.ejs");
+});
+
+app.get("/add-blog", (req, res) => {
+	res.render("./add_blog/add_blog.ejs");
+});
 
 
-app.listen(4000, ()=>{
-    console.log("Started on port 4000");
-})
+
+app.listen(4000, () => {
+	console.log("Started on port 4000");
+});
